901d923ec8a82fa635f14d4439e0a11b3778d01c
Juan Cavani <juanchocavani@gmail.com>
2024-12-19 20:56:53 -0300

    poblacion inicial

--- jMetal-jmetal-5.10/jmetal-example/src/main/java/ae/paleta/colores/ColorPaletteNSGAII.java
+++ jMetal-jmetal-5.10/jmetal-example/src/main/java/ae/paleta/colores/ColorPaletteNSGAII.java
@@ -1,19 +1,14 @@
 package ae.paleta.colores;
 
 import org.uma.jmetal.algorithm.multiobjective.nsgaii.NSGAII;
-import org.uma.jmetal.algorithm.multiobjective.nsgaii.NSGAIIBuilder;
 import org.uma.jmetal.operator.crossover.CrossoverOperator;
 import org.uma.jmetal.operator.crossover.impl.IntegerSBXCrossover;
 import org.uma.jmetal.operator.mutation.MutationOperator;
 import org.uma.jmetal.operator.mutation.impl.IntegerPolynomialMutation;
 import org.uma.jmetal.operator.selection.SelectionOperator;
 import org.uma.jmetal.operator.selection.impl.BinaryTournamentSelection;
-import org.uma.jmetal.problem.Problem;
-import org.uma.jmetal.solution.Solution;
 import org.uma.jmetal.solution.integersolution.IntegerSolution;
-import org.uma.jmetal.util.comparator.DominanceComparator;
 import org.uma.jmetal.util.comparator.RankingAndCrowdingDistanceComparator;
-import org.uma.jmetal.util.evaluator.SolutionListEvaluator;
 import org.uma.jmetal.util.evaluator.impl.SequentialSolutionListEvaluator;
 
 import java.awt.Color;
@@ -23,68 +18,93 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
-
 import javax.imageio.ImageIO;
 
+import smile.clustering.GMeans;
+import smile.clustering.KMeans;
+
 public class ColorPaletteNSGAII extends NSGAII<IntegerSolution> {
+    enum InitialPopulationAlgorithm {
+        DEFAULT,
+        KMEANS,
+        GMEANS
+    }
+
+    private InitialPopulationAlgorithm initialPopulationAlgorithm;
 
     public ColorPaletteNSGAII(
-		ColorPaletteProblem problem,
-		int maxEvaluations,
-		CrossoverOperator<IntegerSolution> crossoverOperator,
-	    MutationOperator<IntegerSolution> mutationOperator,
-	    SelectionOperator<List<IntegerSolution>, IntegerSolution> selectionOperator,
-		int populationSize
-    ) {	
+        ColorPaletteProblem problem,
+        int maxEvaluations,
+        int populationSize,
+        CrossoverOperator<IntegerSolution> crossoverOperator,
+        MutationOperator<IntegerSolution> mutationOperator,
+        SelectionOperator<List<IntegerSolution>, IntegerSolution> selectionOperator,
+        InitialPopulationAlgorithm initialPopulationAlgorithm
+    ) {
         super(
-        	problem,
-        	maxEvaluations, 
-        	populationSize, 
-        	populationSize, 
-        	populationSize, 
-        	crossoverOperator,
-        	mutationOperator, 
-        	selectionOperator, 
-        	new SequentialSolutionListEvaluator<IntegerSolution>()
+            problem,
+            maxEvaluations,
+            populationSize,
+            populationSize,
+            populationSize,
+            crossoverOperator,
+            mutationOperator,
+            selectionOperator,
+            new SequentialSolutionListEvaluator<>()
         );
+        this.initialPopulationAlgorithm = initialPopulationAlgorithm;
     }
+    
+    private double[][] extractPixels(BufferedImage image) {
+        int width = image.getWidth();
+        int height = image.getHeight();
+        double[][] pixels = new double[width * height][3];
+
+        int index = 0;
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                Color color = new Color(image.getRGB(x, y));
+                pixels[index][0] = color.getRed();
+                pixels[index][1] = color.getGreen();
+                pixels[index][2] = color.getBlue();
+                index++;
+            }
+        }
+        return pixels;
+    }
+
 
     @Override
     protected List<IntegerSolution> createInitialPopulation() {
-        // List<IntegerSolution> initialPopulation = new ArrayList<>(getMaxPopulationSize());
-        // BufferedImage image = ((ColorPaletteProblem) getProblem()).getImage();
-        // int k = getMaxPopulationSize();
-        // int numComponents = 4; // R, G, B, Active flag
+        if (initialPopulationAlgorithm == InitialPopulationAlgorithm.DEFAULT) {
+            return super.createInitialPopulation();
+        } else {
+            List<IntegerSolution> initialPopulation = new ArrayList<>(getMaxPopulationSize());
+            BufferedImage image = ((ColorPaletteProblem) getProblem()).image;
+            int maxK = ((ColorPaletteProblem) getProblem()).maxPaletteSize;
 
-        // // Extract pixels from the image
-        // List<int[]> pixels = new ArrayList<>();
-        // for (int y = 0; y < image.getHeight(); y++) {
-        //     for (int x = 0; x < image.getWidth(); x++) {
-        //     Color color = new Color(image.getRGB(x, y));
-        //     pixels.add(new int[]{color.getRed(), color.getGreen(), color.getBlue()});
-        //     }
-        // }
+            double[][] pixelArray = extractPixels(image);
 
-        // // Apply K-Means clustering
-        // KMeansClustering kMeans = new KMeansClustering(k);
-        // List<int[]> centroids = kMeans.cluster(pixels);
+            double[][] centroids;
+            if (initialPopulationAlgorithm == InitialPopulationAlgorithm.GMEANS) {
+                centroids = GMeans.fit(pixelArray, maxK).centroids;
+            } else {
+                centroids = KMeans.fit(pixelArray, maxK).centroids;
+            }
 
-        // // Create initial population based on centroids
-        // for (int[] centroid : centroids) {
-        //     IntegerSolution solution = getProblem().createSolution();
-        //     for (int i = 0; i < k; i++) {
-        //     solution.setVariableValue(i * numComponents, centroid[0]);
-        //     solution.setVariableValue(i * numComponents + 1, centroid[1]);
-        //     solution.setVariableValue(i * numComponents + 2, centroid[2]);
-        //     solution.setVariableValue(i * numComponents + 3, 1); // Active flag
-        //     }
-        //     initialPopulation.add(solution);
-        // }
-
-        // return initialPopulation;
-        return super.createInitialPopulation(); // Default behavior
+            for (double[] centroid : centroids) {
+                IntegerSolution solution = getProblem().createSolution();
+                solution.setVariable(0, (int) centroid[0]);
+                solution.setVariable(1, (int) centroid[1]);
+                solution.setVariable(2, (int) centroid[2]);
+                solution.setVariable(3, 1); // Active flag
+                initialPopulation.add(solution);
+            }
+           
+            return initialPopulation;
+        }
     }
-
+    
     @Override 
     protected boolean isStoppingConditionReached() {
         // Check if the algorithm is stuck in a local optimum
@@ -113,73 +133,76 @@ public class ColorPaletteNSGAII extends NSGAII<IntegerSolution> {
         return super.isStoppingConditionReached();
     }
 
-    public static void main(String[] args) throws Exception {
-    	String imageName = "test1";
-        BufferedImage image = ImageIO.read(ColorPaletteRunner.class.getResourceAsStream(imageName + ".jpg"));
+    public static void main(String[] args) {
+        try {
+            String imageName = "test1";
+            BufferedImage image = ImageIO.read(ColorPaletteNSGAII.class.getResourceAsStream(imageName + ".jpg"));
 
-        int maxPaletteSize = 10;
-        ColorPaletteProblem problem = new ColorPaletteProblem(image, maxPaletteSize);
-        
-        // Create custom initial population
+            int maxPaletteSize = 10;
+            ColorPaletteProblem problem = new ColorPaletteProblem(image, maxPaletteSize);
 
-        CrossoverOperator<IntegerSolution> crossoverOperator = new IntegerSBXCrossover(0.8, 20);
-        MutationOperator<IntegerSolution> mutationOperator = new IntegerPolynomialMutation(1.0 / problem.getNumberOfVariables(), 20);
-        SelectionOperator<List<IntegerSolution>, IntegerSolution> selectionOperator = new BinaryTournamentSelection<IntegerSolution>(new RankingAndCrowdingDistanceComparator<IntegerSolution>());
+            CrossoverOperator<IntegerSolution> crossoverOperator = new IntegerSBXCrossover(0.8, 20);
+            MutationOperator<IntegerSolution> mutationOperator = new IntegerPolynomialMutation(1.0 / problem.getNumberOfVariables(), 20);
+            SelectionOperator<List<IntegerSolution>, IntegerSolution> selectionOperator = new BinaryTournamentSelection<>(new RankingAndCrowdingDistanceComparator<>());
 
-        ColorPaletteNSGAII algorithm = new ColorPaletteNSGAII(
-            problem,
-            5000,
-            crossoverOperator,
-            mutationOperator,
-            selectionOperator,
-            50
-        );
+            ColorPaletteNSGAII algorithm = new ColorPaletteNSGAII(
+                problem,
+                5000,
+                50,
+                crossoverOperator,
+                mutationOperator,
+                selectionOperator,
+                InitialPopulationAlgorithm.DEFAULT
+            );
 
-        long startTime = System.currentTimeMillis();
-        algorithm.run();
-        long endTime = System.currentTimeMillis();
+            long startTime = System.currentTimeMillis();
+            algorithm.run();
+            long endTime = System.currentTimeMillis();
 
-        List<IntegerSolution> population = algorithm.getResult();
-        IntegerSolution bestSolution = population.get(0);
+            List<IntegerSolution> population = algorithm.getResult();
+            IntegerSolution bestSolution = population.get(0);
 
-        List<Integer> variables = bestSolution.getVariables();
-        int numComponents = 4; // R, G, B, Active flag
+            List<Integer> variables = bestSolution.getVariables();
+            int numComponents = 4; // R, G, B, Active flag
 
-        System.out.println("Paleta de colores obtenida:");
-        List<Color> palette = new ArrayList<>();
-        for (int i = 0; i < variables.size(); i += numComponents) {
-            int r = variables.get(i);
-            int g = variables.get(i + 1);
-            int b = variables.get(i + 2);
-            int active = variables.get(i + 3);
+            System.out.println("Generated color palette:");
+            List<Color> palette = new ArrayList<>();
+            for (int i = 0; i < variables.size(); i += numComponents) {
+                int r = variables.get(i);
+                int g = variables.get(i + 1);
+                int b = variables.get(i + 2);
+                int active = variables.get(i + 3);
 
-            if (active == 1) {
-                Color color = new Color(r, g, b);
-                palette.add(color);
-                System.out.println("Color: R=" + r + " G=" + g + " B=" + b);
+                if (active == 1) {
+                    Color color = new Color(r, g, b);
+                    palette.add(color);
+                    System.out.println("Color: R=" + r + ", G=" + g + ", B=" + b);
+                }
             }
+
+            int squareSize = 50;
+            BufferedImage paletteImage = new BufferedImage(squareSize * palette.size(), squareSize, BufferedImage.TYPE_INT_RGB);
+            Graphics g = paletteImage.getGraphics();
+            for (int i = 0; i < palette.size(); i++) {
+                g.setColor(palette.get(i));
+                g.fillRect(i * squareSize, 0, squareSize, squareSize);
+            }
+            g.dispose();
+
+            File outputfile = new File(imageName + "_palette.png");
+            ImageIO.write(paletteImage, "png", outputfile);
+
+            if (Desktop.isDesktopSupported()) {
+                Desktop.getDesktop().open(outputfile);
+            }
+
+            System.out.println("Objective 1 (Distance): " + bestSolution.getObjective(0));
+            System.out.println("Objective 2 (Palette size): " + palette.size());
+
+            long executionTime = endTime - startTime;
+            System.out.println("Total execution time: " + (executionTime / 1000.0) + " seconds");
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-
-        int squareSize = 50;
-        BufferedImage paletteImage = new BufferedImage(squareSize * palette.size(), squareSize, BufferedImage.TYPE_INT_RGB);
-        Graphics g = paletteImage.getGraphics();
-        for (int i = 0; i < palette.size(); i++) {
-            g.setColor(palette.get(i));
-            g.fillRect(i * squareSize, 0, squareSize, squareSize);
-        }
-        g.dispose();
-
-        File outputfile = new File(imageName + "_palette.png");
-        ImageIO.write(paletteImage, "png", outputfile);
-
-        if (Desktop.isDesktopSupported()) {
-            Desktop.getDesktop().open(outputfile);
-        }
-
-        System.out.println("Objetivo 1 (Distancia): " + bestSolution.getObjective(0));
-        System.out.println("Objetivo 2 (Tamaño de paleta): " + palette.size());
-
-        long executionTime = endTime - startTime;
-        System.out.println("Tiempo total de ejecución: " + (executionTime / 1000.0) + " segundos");
     }
 }
